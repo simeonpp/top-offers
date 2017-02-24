f72339459329572f7f1ac2f0c5374880ac7c962b
simeon <petkov.spp@gmail.com>
2017-02-24 23:44:31 +0200

    added login temp page + skelet for data lib

--- AndroidClient/MyOrders/app/src/main/java/com/topoffers/topoffers/login/LoginActivity.java
+++ AndroidClient/MyOrders/app/src/main/java/com/topoffers/topoffers/login/LoginActivity.java
@@ -2,6 +2,9 @@ package com.topoffers.topoffers.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.topoffers.topoffers.R;
 
@@ -11,5 +14,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.addSubmitButtonListener();
+    }
+
+    private void addSubmitButtonListener() {
+        Button submitButton = (Button) this.findViewById(R.id.btn_login_submit);
+        submitButton.setOnClickListener(v -> {
+            String username = ((EditText) this.findViewById(R.id.et_login_username)).getText().toString();
+            String password = ((EditText) this.findViewById(R.id.et_login_password)).getText().toString();
+
+            Toast
+                .makeText(this, username + " - " + password, Toast.LENGTH_LONG)
+                .show();
+        });
     }
 }
